433a68905e6d74655e5adbece83d05c0ee291ff2
Ssaw1212 <118851319+Ssaw1212@users.noreply.github.com>
2026-09-19 20:32:45 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,14 +36,5 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
-
-    compileOnly("io.github.libxposed:api:102.0.0")
-}}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("com.google.android.material:material:1.13.0")
-
     compileOnly("io.github.libxposed:api:102.0.0")
 }

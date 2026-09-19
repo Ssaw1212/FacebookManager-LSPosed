a0348220f7930d838d28a5755765cc913a401335
Ssaw1212 <118851319+Ssaw1212@users.noreply.github.com>
2026-09-19 20:43:38 +0300

    Update ModuleMain.kt

--- app/src/main/java/com/ssaw1212/facebookmanager/ModuleMain.kt
+++ app/src/main/java/com/ssaw1212/facebookmanager/ModuleMain.kt
@@ -2,9 +2,13 @@ package com.ssaw1212.facebookmanager
 
 import android.app.Application
 import android.content.Context
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
 import io.github.libxposed.api.XposedInterface
 import io.github.libxposed.api.XposedModule
+import io.github.libxposed.api.XposedModuleInterface.ModuleLoadedParam
+import io.github.libxposed.api.XposedModuleInterface.PackageLoadedParam
 
 class ModuleMain : XposedModule() {
 
@@ -14,32 +18,54 @@ class ModuleMain : XposedModule() {
     }
 
     override fun onModuleLoaded(param: ModuleLoadedParam) {
-        log(Log.INFO, TAG, "Module loaded: ${param.processName}")
+        log(
+            Log.INFO,
+            TAG,
+            "Module loaded: ${param.getProcessName()}"
+        )
     }
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onPackageLoaded(param: PackageLoadedParam) {
-        if (param.packageName != TARGET) return
+        if (param.getPackageName() != TARGET) return
 
-        log(Log.INFO, TAG, "Facebook loaded")
+        log(
+            Log.INFO,
+            TAG,
+            "Facebook loaded: ${param.getPackageName()}"
+        )
 
         try {
-            val method = Application::class.java.getDeclaredMethod(
+            val attach = Application::class.java.getDeclaredMethod(
                 "attach",
                 Context::class.java
             )
 
-            hook(method)
+            hook(attach)
                 .setId("facebook_application_attach")
-                .intercept(XposedInterface.Hooker { chain ->
-                    val result = chain.proceed()
-                    log(Log.INFO, TAG, "Facebook Application.attach observed")
-                    result
-                })
+                .intercept(
+                    XposedInterface.Hooker { chain ->
+                        val result = chain.proceed()
+
+                        log(
+                            Log.INFO,
+                            TAG,
+                            "Facebook Application.attach observed"
+                        )
+
+                        result
+                    }
+                )
 
             log(Log.INFO, TAG, "Base hook installed")
 
         } catch (t: Throwable) {
-            log(Log.ERROR, TAG, "Hook installation failed", t)
+            log(
+                Log.ERROR,
+                TAG,
+                "Hook installation failed",
+                t
+            )
         }
     }
 }

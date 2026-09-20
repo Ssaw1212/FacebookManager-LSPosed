80ed51079792324f7848dc4b72a5690c0cc6a0c2
Ssaw1212 <ssaw1212@gmail.com>
2026-09-20 20:53:18 +0300

    Add DexKit and update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ssaw1212.facebookmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.0-modern"
+        versionCode = 10
+        versionName = "2.0.0"
     }
 
     buildFeatures {
@@ -35,4 +35,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     compileOnly("io.github.libxposed:api:102.0.0")
+    implementation("org.luckypray:dexkit:2.0.6")
 }
